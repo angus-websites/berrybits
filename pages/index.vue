21ac4f4e1ffb9f4be0c8659ea68b7f78847221ac
Angus Goody <angus.goody@gmail.com>
2024-01-17 19:29:10 +0000

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <PageContainer>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+  </PageContainer>
+</template>
+<script setup lang="ts">
+
+</script>
